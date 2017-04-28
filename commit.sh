cc733c71d1c9f0f0ba108408127215520feef470
raygoat <diverse@oob.dk>
2017-04-28 09:30:51 +0000

    Fjernet netgear, da det ikke virker i en iFrame

---
+++ commit.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+# Skifter til bruger Home Assistant
+
+#cd /home/homeassistant/.homeassistant
+git add -A
+if [ $# -eq 0 ]
+then
+  git commit -m "Ingen kommentar."
+else
+  git commit -m "$*"
+fi
+git status
+git push origin master
